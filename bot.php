175d49dc45af082b65723205e4bf7e144028c488
ThereFox <59343016+ThereFox@users.noreply.github.com>
2021-04-27 15:11:55 +0300

    321
    312

---
+++ bot.php
@@ -0,0 +1,37 @@
+<?php
+    include('vendor/autoload.php'); //библиотека
+    use Telegram\Bot\Api;
+
+    $telegram = new Api('1727319575:AAHi8f1hNrRXZDjRLTskuRvd5S8QF-7n5qM'); //токен, полученный у BotFather
+    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+
+    $text = $result["message"]["text"]; //Текст сообщения
+    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $keyboard = [["Сайты"],["Картинка"],["Расписание"]]; //Клавиатура
+
+    if($text){
+         if ($text == "/start") {
+            $reply = "Привет {$name}!";
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        }
+        elseif ($text == "/help")
+        {
+            $reply = "Что ты хочешь узнать?";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }
+        /*elseif ($text == "Сайты")
+        {
+            $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+        }*/
+        else
+        {
+        	$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
+        	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
+        }
+    }else{
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    }
+?>
